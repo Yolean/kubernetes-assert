8373b1758d5965a96ae849f137a28dbb945080f6
Staffan Olsson <staffan@repos.se>
2017-06-14 11:10:12 +0200

    Variable interpolation didn't work, or not this way

--- slack-secret.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo -n Please enter the Slack Incoming WebHooks URL:
-read -s slack_alerts_webhook_url
-echo
-kubectl create secret generic slack-secret --namespace=monitoring --from-literal=slack_alerts_webhook_url=$slack_alerts_webhook_url
