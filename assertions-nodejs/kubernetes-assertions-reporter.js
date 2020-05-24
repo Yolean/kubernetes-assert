31a22d8f6eb480bab06fb7f9dd6cf0799517522a
Staffan Olsson <staffan@repos.se>
2020-05-24 19:14:14 +0200

    Reports test results on localhost:9090

--- assertions-nodejs/kubernetes-assertions-reporter.js
+++ assertions-nodejs/kubernetes-assertions-reporter.js
@@ -1,47 +1,62 @@
 console.log('Reporter loaded');
 
+const http = require('http');
 
-const http2 = require('http2');
-const {
-  HTTP2_HEADER_METHOD,
-  HTTP2_HEADER_PATH,
-  HTTP2_HEADER_STATUS,
-  HTTP2_HEADER_CONTENT_TYPE
-} = http2.constants;
+const client = require('prom-client');
+const register = client.register;
 
+const assertions_failed = new client.Counter({
+  name: 'assertions_failed',
+  help: 'inc\'d for every onTestResult numFailingTests',
+});
+
+const tests_total = new client.Counter({
+  name: 'tests_total',
+  help: 'inc\'d for every onRunComplete numTotalTests'
+})
+
+const test_suites_total = new client.Counter({
+  name: 'test_suites_total',
+  help: 'inc\'d for every onRunComplete numTotalTestSuites'
+})
+
+/**
+ * TODO can we somehow exit on non-watch jest runs?
+ */
 class MetricsServer {
 
-  constructor({ port }) {
-    console.log('metrics server init', port);
+  constructor({ port, getMetrics }) {
+    console.log('metrics server init', port, typeof getMetrics);
     this.port = port;
+    this.getMetrics = getMetrics;
   }
 
   start() {
-    this.server = http2.createServer(this.serverOptions);
-    this.server.on('stream', (stream, headers, flags) => {
-      const method = headers[HTTP2_HEADER_METHOD];
-      const path = headers[HTTP2_HEADER_PATH];
-      // ...
-      stream.respond({
-        [HTTP2_HEADER_STATUS]: 200,
-        [HTTP2_HEADER_CONTENT_TYPE]: 'text/plain'
-      });
-      stream.write('hello ');
-      stream.end('world');
+    const getMetrics = this.getMetrics;
+    this.server = http.createServer((req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(getMetrics());
+    });
+    this.server.on('clientError', (err, socket) => {
+      socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
     });
     this.server.listen(this.port);
   }
 
 }
 
-class MyCustomReporter {
+const server = new MetricsServer({
+  port: 9090,
+  getMetrics: () => register.metrics()
+});
+
+server.start();
+
+class MetricsReporter {
 
   constructor(globalConfig, options) {
     this._globalConfig = globalConfig;
     this._options = options;
-    //console.log('Reporter constructed');
-    this.server = new MetricsServer({ port: 9090 });
-    this.server.start();
   }
 
   onRunStart() {
@@ -51,15 +66,18 @@ class MyCustomReporter {
   onTestStart() {
     //console.log('onTestStart', arguments);
   }
-  
-  onTestResult(test, testResult, aggregatedResult) {
-    console.log('onTestResult', testResult);
-  }
 
   onRunComplete(contexts, results) {
-    //console.log('Complete!', contexts, results);
-    // TODO exit server
+    //console.log('onRunComplete', contexts, results);
+    test_suites_total.inc(results.numTotalTestSuites);
+    tests_total.inc(results.numTotalTests);
   }
+  
+  onTestResult(test, testResult, aggregatedResult) {
+    assertions_failed.inc(testResult.numFailingTests);
+    //console.log('onTestResult', testResult);
+  }
+
 }
 
-module.exports = MyCustomReporter;
+module.exports = MetricsReporter;
