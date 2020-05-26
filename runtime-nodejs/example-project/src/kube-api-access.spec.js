8b92aed9fd63f17181e2ff49dde3dd0f6b33dc3f
Staffan Olsson <staffan@repos.se>
2020-05-26 10:14:02 +0200

    Now we can skaffold dev a downstream project; TODO workdir issues

---
+++ runtime-nodejs/example-project/src/kube-api-access.spec.js
@@ -0,0 +1,11 @@
+describe("kube api access", () => {
+
+  xit("Uses a library in this project's package.json", () => {
+    // TODO
+  });
+
+  it("Can locate the pod's mounted credentials", () => {
+    expect(true).toEqual(false);
+  });
+
+});
