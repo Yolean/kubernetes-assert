dddf5b4c5d6178d30f38aa5bf988ae0fed9491cd
Staffan Olsson <staffan@repos.se>
2020-06-08 17:35:03 +0200

    Adds an endpoint to trigger rerun of all spec files
    I failed to find an API for non-iteractive watch that cleanly triggers rerun
    like keypress a does in interactive mode. Fake changes will have to do.

--- runtime-nodejs/jest.kubernetes-assertions-reporter.js
+++ runtime-nodejs/jest.kubernetes-assertions-reporter.js
@@ -1,7 +1,4 @@
-// TODO a https://jestjs.io/docs/en/watch-plugins might be better suited to the task
-
-console.log('Assert reporter loaded, see github.com/Yolean/kubernetes-assert');
-
+const fs = require('fs');
 const http = require('http');
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 9091;
@@ -44,6 +41,38 @@ const assert_files_seen = new client.Gauge({
   help: 'Unique spec file paths that have been seen in onTestResult'
 });
 
+class SpecFilesTracker {
+
+  constructor() {
+    this._pathsSeen = {};
+  }
+
+  pathSeen(path) {
+    if (!this._pathsSeen[path]) {
+      this._pathsSeen[path] = {};
+      assert_files_seen.inc();
+      console.log('New path', path);
+    }
+  }
+
+  modify(path) {
+    const insignificant = '\n';
+    fs.appendFile(path, insignificant, 'utf8', () => {
+      console.log('Modified', path);
+    });
+  }
+
+  modifyAll() {
+    const modify = this.modify.bind(this);
+    Object.keys(this._pathsSeen).map(path => {
+      modify(path);
+    });
+  }
+
+}
+
+const tracker = new SpecFilesTracker();
+
 class MetricsServer {
 
   constructor({ port, getMetrics }) {
@@ -51,11 +80,24 @@ class MetricsServer {
     this.getMetrics = getMetrics;
   }
 
+  serveMetrics(res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(this.getMetrics());
+  }
+
+  serveRerun(res) {
+    tracker.modifyAll(); // Would there be a point with waiting?
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('{}');
+  }
+
   start() {
-    const getMetrics = this.getMetrics;
     this.server = http.createServer((req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(getMetrics());
+      //console.log('req', req.url, req.headers);
+      if ('/metrics' == req.url) return this.serveMetrics(res);
+      if ('POST' == req.method && '/rerun' == req.url) return this.serveRerun(res);
+      res.writeHead(404, { 'Content-Length': '0' });
+      res.end();
     });
     this.server.on('clientError', (err, socket) => {
       socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
@@ -82,7 +124,6 @@ class MetricsReporter {
   constructor(globalConfig, options) {
     this._globalConfig = globalConfig;
     this._options = options;
-    this._pathsSeen = {};
   }
 
   onRunStart() {
@@ -109,11 +150,7 @@ class MetricsReporter {
 
   onTestResult(test, testResult, aggregatedResult) {
     //console.log('onTestResult', testResult);
-    const path = testResult.testFilePath;
-    if (!this._pathsSeen[path]) {
-      this._pathsSeen[path] = {};
-      assert_files_seen.inc();
-    }
+    tracker.pathSeen(testResult.testFilePath);
   }
 
 }
